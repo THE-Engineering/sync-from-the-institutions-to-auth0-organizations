85e2662549ce1b20f43f862fe9e620bb017f5dad
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-03-01 17:01:16 +0000

    Mocha

--- tests/application/validate.spec.mjs
+++ tests/application/validate.spec.mjs
@@ -1,13 +1,85 @@
 import {
   expect
 } from 'chai'
-import {
+import validate, {
   hasChanged,
   hasRemoved,
   hasAdded
 } from '#application/validate'
 
 describe('`#application/validate`', () => {
+  describe('validate()', () => {
+    describe('Organizations are changed', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'X' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are not changed', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'C' }] }
+
+      it('returns false', () => expect(hasChanged(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are removed', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are not removed', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'C' }] }
+
+      it('returns true', () => expect(validate(organizations, institutions)).to.be.true)
+    })
+
+    describe('Organizations are added', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'C' }, { iid: 'D', name: 'D' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are not added', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'C' }] }
+
+      it('returns true', () => expect(validate(organizations, institutions)).to.be.true)
+    })
+
+    describe('Organizations are changed or added', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'B' }, { iid: 'C', name: 'X' }, { iid: 'D', name: 'D' }] }
+
+      it('returns true', () => expect(hasChanged(organizations, institutions)).to.be.true)
+    })
+
+    describe('Organizations are changed or removed', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'B', name: 'X' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are removed or added', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'C', name: 'C' }, { iid: 'D', name: 'D' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+
+    describe('Organizations are changed or removed or added', () => {
+      const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
+      const institutions = { rows: [{ iid: 'A', name: 'A' }, { iid: 'C', name: 'X' }, { iid: 'D', name: 'D' }] }
+
+      it('returns false', () => expect(validate(organizations, institutions)).to.be.false)
+    })
+  })
+
   describe('hasChanged()', () => {
     describe('Organizations are changed', () => {
       const organizations = [{ name: 'A', display_name: 'A' }, { name: 'B', display_name: 'B' }, { name: 'C', display_name: 'C' }]
