341c6b210072bbdc6ee03785927a68591a3d12b8
Joao Silva <joaorosilva@gmail.com>
2024-06-11 09:53:00 +0100

    Fix array.includes call

--- utils/throw-on-error.mjs
+++ utils/throw-on-error.mjs
@@ -1,8 +1,11 @@
 export const throwOnError = async (response, acceptableCodes = []) => {
   if(!response.ok) {
-    console.error(`Non-Ok response status ${response.status}(${response.statusText}) from ${response.url}. Trace: ${(new Error()).stack}`);
-    console.error(await response.text());
-    if(!acceptableCodes || !acceptableCodes.contains(response.status))
+    if(acceptableCodes instanceof Array && acceptableCodes.includes(response.status)) {
+      console.log(`Got non-ok response status ${response.status}(${response.statusText}) from ${response.url}, but code is within acceptable codes: ${acceptableCodes}. Trace: ${(new Error()).stack}`);
+    } else {
+      console.error(`Non-ok response status ${response.status}(${response.statusText}) from ${response.url}. Trace: ${(new Error()).stack}`);
+      console.error(await response.text());
       throw new Error(`Something went wrong fetching from ${response.url}!`);
+    }
   }
 }
